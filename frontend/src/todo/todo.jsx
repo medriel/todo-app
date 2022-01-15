13ae04f65e6e23e35fe3588f4adb9228c2bc349a
Adriel Medeiros <adriel.souza397@gmail.com>
2022-01-15 09:12:18 -0300

    Consulta e Exclusão de TODOs

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -14,6 +14,15 @@ export default class Todo extends Component {
     this.state = { description: '', list: [] }
     this.handleChange = this.handleChange.bind(this)
     this.handleAdd = this.handleAdd.bind(this)
+
+    this.handleRemove = this.handleRemove.bind(this)
+
+    this.refresh()
+  }
+
+  refresh() {
+    axios.get(`${URL}? sort=-createdAt`)
+      .then(resp => this.setState({ ...this.state, description: '', list: resp.data }))
   }
 
   handleChange(e) {
@@ -23,7 +32,12 @@ export default class Todo extends Component {
   handleAdd() {
     const description = this.state.description
     axios.post(URL, { description })
-      .then(resp => console.log('funcionou'))
+      .then(resp => this.refresh())
+  }
+
+  handleRemove(todo) {
+    axios.delete(`${URL}/${todo._id}`)
+      .then(resp => this.refresh())
   }
 
   render() {
@@ -35,7 +49,10 @@ export default class Todo extends Component {
           handleChange={this.handleChange}
           handleAdd={this.handleAdd}
         />
-        <TodoList />
+        <TodoList
+          list={this.state.list}
+          handleRemove={this.handleRemove}
+        />
       </div>
     )
   }
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,41 @@
 import React from 'react'
+import IconButton from '../template/iconButton'
 
-export default props => (
-  <div>
-    <h1>List</h1>
-  </div>
-)
+export default props => {
+
+  const renderRows = () => {
+    const list = props.list || []
+    return list.map(todo => (
+      <tr key={todo._id}>
+
+        <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+
+        <td>
+
+          <IconButton
+            style='danger'
+            icon='trash-o'
+            hide={!todo.done}
+            onClick={() => props.handleRemove(todo)}
+          ></IconButton>
+
+        </td>
+
+      </tr>
+    ))
+  }
+
+  return (
+    <table className='table'>
+      <thead>
+        <tr>
+          <th>Descrição</th>
+          <th className='tableActions'>Ações</th>
+        </tr>
+      </thead>
+      <tbody>
+        {renderRows()}
+      </tbody>
+    </table>
+  )
+}
