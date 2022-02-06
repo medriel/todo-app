ab57820a8443b712a57e9c46ccd85bb52d40955b
adriel.souza397@gmail.com <adriel.souza397@gmail.com>
2022-02-06 11:21:06 -0300

    Configurando os Reducers

---
+++ frontend/src/main/reducers.js
@@ -0,0 +1,22 @@
+import { combineReducers } from 'redux'
+
+const rootReducer = combineReducers({
+  todo: () => ({
+    description: 'Ler livro',
+    list: [{
+      _id: 1,
+      description: 'aaaaaaaaaaaaaaaaaaaaaaa',
+      done: true
+    }, {
+      _id: 2,
+      description: 'bbbbbbbbbbbbbbbbbbbb',
+      done: false
+    },
+    {
+      _id: 3,
+      description: 'ccccccccccccccccc',
+      done: true
+    }
+    ]
+  })
+})
