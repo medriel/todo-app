157b4266b8cee0749f46babc52b68dddf6fac154
Adriel Medeiros <adriel.souza397@gmail.com>
2022-02-08 19:42:06 -0300

    Melhorando a Action de Pesquisa

--- frontend/src/todo/todoActions.js
+++ frontend/src/todo/todoActions.js
@@ -8,10 +8,11 @@ export const changeDescription = event => ({
 })
 
 export const search = () => {
-  const request = axios.get(`${URL}?sort=-createdAt`)
-  return {
-    type: 'TODO_SEARCHED',
-    payload: request
+  return (dispatch, getState) => {
+    const description = getState().todo.description
+    const search = description ? `&description__regex=/${description}/` : ''
+    const request = axios.get(`${URL}?sort=-createdAt${search}`)
+      .then(resp => dispatch({ type: 'TODO_SEARCHED', payload: resp.data }))
   }
 }
 
@@ -56,5 +57,8 @@ export const remove = (todo) => {
 }
 
 export const clear = () => {
-  return { type: 'TODO_CLEAR' }
+  return [
+    { type: 'TODO_CLEAR' },
+    search()
+  ]
 }
--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -42,7 +42,7 @@ class TodoForm extends Component {
           <IconButton style='primary' icon='plus'
             onClick={() => add(description)}></IconButton>
           <IconButton style='info' icon='search'
-            onClick={() => search()}></IconButton>
+            onClick={search}></IconButton>
           <IconButton style='default' icon='close'
             onClick={this.props.clear}></IconButton>
         </Grid>
